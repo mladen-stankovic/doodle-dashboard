e814c79001fd2d0d4254e5c3ab3ea332f35d0c45
Mladen Stankovic <stankovic.mladen@gmail.com>
2020-09-23 17:56:59 +0200

    DD-1 initial project structure
    (cherry picked from commit 1c7d67bc67258b3508cbe71532ecb0b929388976)

---
+++ src/main/java/com/doodle/doodledashboard/DoodleDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.doodle.doodledashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoodleDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoodleDashboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/doodle/doodledashboard/DoodleDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doodle.doodledashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoodleDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
