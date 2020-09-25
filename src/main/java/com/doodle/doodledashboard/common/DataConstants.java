b639a9859c37ca72d301505a25e5500d5e00da9d
Mladen Stankovic <stankovic.mladen@gmail.com>
2020-09-25 08:02:16 +0200

    DD-7 Poll service-> findCreatedAfterDate unit test

--- src/main/java/com/doodle/doodledashboard/common/DataConstants.java
+++ src/main/java/com/doodle/doodledashboard/common/DataConstants.java
@@ -8,4 +8,6 @@ public class DataConstants {
     public static final String TEST_INITIATOR_1 = "mh+sample@doodle.com";
     public static final String TEST_INITIATOR_2 = "test+sample@doodle.com";
     public static final String SEARCH_TERM = "you";
+    public static final String TEST_DATE_IN_THE_PAST = "1483966099216";
+    public static final String TEST_DATE_IN_THE_FUTURE = "2232135918000";
 }
--- src/test/java/com/doodle/doodledashboard/unit/PollServiceUnitTests.java
+++ src/test/java/com/doodle/doodledashboard/unit/PollServiceUnitTests.java
@@ -12,14 +12,13 @@ import org.bson.Document;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.UUID;
 
-import static com.doodle.doodledashboard.common.DataConstants.POLLS_COLLECTION;
+import static com.doodle.doodledashboard.common.DataConstants.TEST_DATE_IN_THE_PAST;
 import static org.mockito.Mockito.*;
 
 /**
@@ -34,10 +33,7 @@ public class PollServiceUnitTests {
     @Mock
     private MongoTemplate mongoTemplate;
 
-    private Query givenUserHasPolls() {
-        Query query = new Query();
-        query.addCriteria(Criteria.where("initiator.email").is(DataConstants.TEST_INITIATOR_1));
-
+    private void givenUserHasPolls() {
         List<Document> documents = Lists.newArrayList();
         for (int i = 0; i < 3; i++) {
             Document document = new Document();
@@ -46,29 +42,30 @@ public class PollServiceUnitTests {
             document.put("initiator", initiator);
             documents.add(document);
         }
-        when(mongoTemplate.find(query, Document.class, DataConstants.POLLS_COLLECTION)).thenReturn(documents);
-
-        return query;
+        when(mongoTemplate.find(any(Query.class), eq(Document.class), any(String.class))).thenReturn(documents);
     }
 
-    private Query givenSearchByTitleHasResults() {
-        Query query = new Query();
-        query.addCriteria(Criteria.where("title").regex(".*" + DataConstants.SEARCH_TERM + ".*"));
-
+    private void givenSearchByTitleHasResults() {
         List<Document> documents = Lists.newArrayList();
         for (int i = 0; i < 3; i++) {
             Document document = new Document();
             document.put("title", UUID.randomUUID().toString() + DataConstants.SEARCH_TERM + UUID.randomUUID().toString());
             documents.add(document);
         }
-        when(mongoTemplate.find(query, Document.class, DataConstants.POLLS_COLLECTION)).thenReturn(documents);
+        when(mongoTemplate.find(any(Query.class), eq(Document.class), any(String.class))).thenReturn(documents);
+    }
 
-        return query;
+    private void givenThereArePollsAfterProvidedDate() {
+        List<Document> documents = Lists.newArrayList();
+        Document document = new Document();
+        document.put("initiated", Long.parseLong(TEST_DATE_IN_THE_PAST) + 10000000);
+        documents.add(document);
+        when(mongoTemplate.find(any(Query.class), eq(Document.class), any(String.class))).thenReturn(documents);
     }
 
     @Test
     public void givenUserHasPolls_whenSearchingForPolls_checkResponse() {
-        Query query = givenUserHasPolls();
+        givenUserHasPolls();
 
         List<Document> documents = pollService.findByInitiatorEmail(DataConstants.TEST_INITIATOR_1);
         Assertions.assertNotNull(documents);
@@ -78,12 +75,12 @@ public class PollServiceUnitTests {
             Assertions.assertEquals(((Document)d.get("initiator")).get("email"), DataConstants.TEST_INITIATOR_1);
         });
 
-        verify(mongoTemplate, times(1)).find(query, Document.class, POLLS_COLLECTION);
+        verify(mongoTemplate, times(1)).find(any(Query.class), eq(Document.class), any(String.class));
     }
 
     @Test
     public void givenSearchByTitleHasResults_whenSearchingForPolls_checkResponse() {
-        Query query = givenSearchByTitleHasResults();
+        givenSearchByTitleHasResults();
 
         List<Document> documents = pollService.searchByTitle(DataConstants.SEARCH_TERM);
         Assertions.assertNotNull(documents);
@@ -93,6 +90,22 @@ public class PollServiceUnitTests {
             Assertions.assertTrue(d.getString("title").contains(DataConstants.SEARCH_TERM));
         });
 
-        verify(mongoTemplate, times(1)).find(query, Document.class, POLLS_COLLECTION);
+        verify(mongoTemplate, times(1)).find(any(Query.class), eq(Document.class), any(String.class));
+    }
+
+    @Test
+    public void givenThereArePollsAfterProvidedDate_whenSearchingForPolls_checkResponse() {
+        givenThereArePollsAfterProvidedDate();
+
+        Long testDateInThePast = Long.parseLong(TEST_DATE_IN_THE_PAST);
+        List<Document> documents = pollService.findCreatedAfterDate(testDateInThePast);
+        Assertions.assertNotNull(documents);
+        Assertions.assertNotEquals(documents.size(), 0);
+        documents.forEach(d -> {
+            Assertions.assertNotNull(d.get("initiated"));
+            Assertions.assertTrue(d.getLong("initiated") > testDateInThePast);
+        });
+
+        verify(mongoTemplate, times(1)).find(any(Query.class), eq(Document.class), any(String.class));
     }
 }
